ad97462b76b736dc4b92d77f31c1f9575a7c0cdb
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2026-08-30 11:55:54 +0200

    chore(ci): Configured chromatic

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from 'storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: "Shko Online's Managed Identity Wizard for Power Platform ToolBox",
+    brandUrl: 'https://github.com/Shko-Online/ManagedIdentityWizardPPTB',
+    brandImage: './ShkoOnline92x32.svg',
+    brandTarget: '_self',
+});
